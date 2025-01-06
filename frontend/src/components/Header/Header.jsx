c406901d30d7bd25b5ed916e6ffcc615291e5c19
Akanksha Salke <akankshasalke@gmail.com>
2025-01-06 22:13:33 +0530

    Initial commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Purely Delicious, Purely Vegetarian.. Enjoy a variety of mouthwatering dishes made with the best ingredients and expert care—one unforgettable meal at a time!</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
